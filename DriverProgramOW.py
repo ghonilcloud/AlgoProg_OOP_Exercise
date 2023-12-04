5b4fc0190595a1b8ad9e4ff5354ac69bead3278e
ghonilcloud <144786178+ghonilcloud@users.noreply.github.com>
2023-12-04 16:19:18 +0700

    Update DriverProgramOW.py

--- DriverProgramOW.py
+++ DriverProgramOW.py
@@ -1,14 +1,44 @@
 from ClassDefOW import ButcherOW
 
 def listcreator_OW():
-    foodlist = list()
+    foodlist = []
 
     while True:
         num_items = int(input("Enter amount of items: "))
         if num_items >= 1:
             break
 
+    itemadd = 0
     for items in range(num_items):
-        name_item = input("Enter the name of the item: )")
+        itemadd += 1
+        print(f'Add Item #{itemadd}')
+
+        name_item = input("Enter the name of the item: ")
         while True:
             num_pounds = eval(input("Enter the amount of pounds: "))
+            if num_pounds >= 1:
+                break
+        foodlist.append(ButcherOW(name_item, num_pounds))
+
+    return foodlist
+
+def reveal_foodlist_OW(list):
+    for item in list:
+            print(f'Item: {item.getName_OW()}')
+            print(f'Amount Ordered: {item.getAmount_OW()}')
+            print(f'Price Per Pound: {item.getStandardpriceperpound_OW()}')
+            print(f'Price of order: {item.getCalculatedPrice_OW()}')
+            print(f'\n')
+
+def calculate_total_OW(list):
+    total = []
+    for item in list:
+        total.append(item.pricelisttotal_OW())
+    return sum(total)
+
+def main_OW():
+    list = listcreator_OW()
+    reveal_foodlist_OW(list)
+    return calculate_total_OW(list)
+
+main_OW()
